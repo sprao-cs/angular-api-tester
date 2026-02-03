48103be8ede08ded97e64c17a64661bf37eebc3e
Prasanth Rao <prasanthraos1798@gmail.com>
2026-02-03 19:11:26 +0530

    Remove RouterOutlet import from App component
    Removed unused RouterOutlet import from App component.

--- api-test-app/src/app/app.ts
+++ api-test-app/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ApiTesterComponent } from './components/api-tester/api-tester.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ApiTesterComponent],
+  imports: [ApiTesterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
 }
+
